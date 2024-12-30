c1668cf9c1640b3b2f2a108ecb8519b391fc7793
CodeGA366 <codega366@gmail.com>
2024-12-30 00:32:08 -0600

    config authForm

--- roster-radar-frontend/src/components/AuthForm.tsx
+++ roster-radar-frontend/src/components/AuthForm.tsx
@@ -14,7 +14,7 @@ const AuthForm: React.FC = () => {
     setError('');
 
     try {
-      const url = isLogin ? 'http://localhost:1000/api/login' : 'http://localhost:1000/api/register';
+      const url = isLogin ? 'https://roster-x1pq.onrender.com//api/login' : 'https://roster-x1pq.onrender.com//api/register';
       const response = await axios.post(url, { username, password, email: isLogin ? undefined : email }); // Send email only if registering
 
       if (isLogin) {
