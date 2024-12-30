f010c18c62a6e65ef4cc6cfd705a5d22dcc4784b
CodeGA366 <codega366@gmail.com>
2024-12-29 20:58:00 -0600

    db.ts config

--- server/src/db.ts
+++ server/src/db.ts
@@ -5,19 +5,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 //db connection
+const isRender = process.env.DATABASE_URL !== undefined;
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT || '5432'),
-});
+const pool = isRender
+    ? new Pool({
+          connectionString: process.env.DATABASE_URL, // Use the Render database URL
+          ssl: {
+              rejectUnauthorized: false, // Enable SSL if required by Render
+          },
+      })
+    : new Pool({
+          user: process.env.DB_USER,
+          host: process.env.DB_HOST,
+          database: process.env.DB_NAME,
+          password: process.env.DB_PASSWORD,
+          port: parseInt(process.env.DB_PORT || '5432'),
+      });
 
 pool.on('error', (err) => {
     console.error('Unexpected error on idle client', err);
     process.exit(-1);
 });
 
-//export
 export { pool };
