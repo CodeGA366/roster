7f26eef2cfdaa7245ad4644940b694d3853e5782
CodeGA366 <codega366@gmail.com>
2024-12-29 22:36:40 -0600

    config changes

--- server/src/initDataBase.ts
+++ server/src/initDataBase.ts
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const initDatabase = async () => {
+  console.log('Environment variables:', process.env);
   console.log('DB_HOST:', process.env.DB_HOST);
   console.log('DB_USER:', process.env.DB_USER);
   console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
